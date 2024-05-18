385ca89e921647442dad501633c392363296e7ee
shahadatarifin <shahadat.arifin.onim1122@gmail.com>
2024-05-18 19:14:25 +0600

    update

---
+++ python.api/IIT_Madras.api/10030.3.WhileLoop.py
@@ -0,0 +1,13 @@
+'''
+problem 3: Reversse the digit in the given number
+
+input           output
+1234            4321
+123456789       987654321
+0               0
+8               8
+-1234           -4321
+
+'''
+
+n = int(input("Enter a number: "))
