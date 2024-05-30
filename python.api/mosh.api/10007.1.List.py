e92e45cca673c131b513b34e22fc14a3d10a7da4
Shahadat Arifn <shahadat.arifin.onim1122@gmail.com>
2024-05-30 14:22:49 +0600

    update

---
+++ python.api/mosh.api/10007.1.List.py
@@ -0,0 +1,20 @@
+# WAP to remove the duplicates
+l = [1, 4, 4, 9, 69, 69, 3, 5, 1]
+
+for i in l:
+    if l.count(i) > 1:
+        l.remove(i)
+
+print(l)
+
+
+
+
+
+# Another solution 
+m = []
+
+for i in l:
+    if i not in m:
+        m.append(i)
+print(m)
--- python.api/mosh.api/z.py
+++
@@ -1,6 +0,0 @@
-f = [5, 2, 5, 2, 2]
-
-for i in f:
-    print("")
-    for j in range(i):
-        print("x", end = '')
