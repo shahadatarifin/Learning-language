1db79e9aa1750040da60b8179615888535a00f05
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2024-12-28 16:43:41 +0530

    update

--- Java/X_10001_1_HelloWorld.java
+++ Java/X_10001_1_HelloWorld.java
@@ -1,6 +1,6 @@
 package Java;
 
-public class X_10001_HelloWorld {
+public class X_10001_1_HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
     }
