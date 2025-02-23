733d2de8ac51dbad913b4e784e6c8d32b062540f
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2025-02-23 15:45:53 +0530

    Delete Java/Harry/practice set directory
    delete

--- Java/Harry/practice set/Chapter-1/Questions_c1.java
+++
@@ -1,9 +0,0 @@
-/*
-1. Write a program to sum three numbers in java
-
-2. Write a java program which asks the user to enter his name and greets them with “hello <name>, have a goal day” text
-
-3. Write a java program to convert kilometres to miles
-
-4. Write a java program to detect whether a number entered by the user is integer or not
-*/
