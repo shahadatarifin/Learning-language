455954dba9e425b1ed45bf267b492b110383fda1
Shahadat Arifn <149145008+shahadatarifin@users.noreply.github.com>
2025-02-24 23:23:14 +0530

    update

---
+++ Java/practice set/Chapter-4/Questions.java
@@ -0,0 +1,18 @@
+/*
+1. What will be the output of this program:
+    int a = 10
+    if (a = 11)
+        System.out.println("I am 11");
+    else
+        System.out.println("I am not 11);
+
+2.  Write a program to find out whether a student is pass or fail; if it requires total 40%
+    and at least 33% in each subject to pass. Assume 3 subjects and take marks as an input
+    from the user
+
+3.  Calculate income tax for paid by an employee to the government as per the slales mentioned below:
+    income slale        tax
+    2.5L - 5.0L         5%
+    5.0L - 10.0L        20%
+    above  10.0L        30%
+ */
